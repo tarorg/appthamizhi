ea0785582a4bd9d45ca1f0afcc4d854b52c443f6
wetarorg <tarframework@gmail.com>
2024-09-16 11:10:35 +0530

    t8

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'server', // Change this from 'static' to 'server'
   vite: {
     resolve: {
       alias: {
@@ -12,6 +13,10 @@ export default defineConfig({
       }
     },
     define: {
+      'import.meta.env.PUBLIC_CLOUDFLARE_ACCOUNT_ID': JSON.stringify(process.env.PUBLIC_CLOUDFLARE_ACCOUNT_ID),
+      'import.meta.env.PUBLIC_R2_ACCESS_KEY_ID': JSON.stringify(process.env.PUBLIC_R2_ACCESS_KEY_ID),
+      'import.meta.env.PUBLIC_R2_SECRET_ACCESS_KEY': JSON.stringify(process.env.PUBLIC_R2_SECRET_ACCESS_KEY),
+      'import.meta.env.PUBLIC_R2_BUCKET_NAME': JSON.stringify(process.env.PUBLIC_R2_BUCKET_NAME),
       'import.meta.env.PUBLIC_R2_DOMAIN': JSON.stringify(process.env.PUBLIC_R2_DOMAIN),
     },
   }
